0cd76ac0cdbacec40d3c3e3bb4bcb3697696dd4f
Greyo0 <arthurrodriguesgrey@gmail.com>
2026-04-19 22:17:40 -0300

    .

---
+++ CursoUdemy/Lista5_Classes/Pessoas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lista5_Classes
+{
+    public class Pessoas
+    {
+
+        public string nome = Console.ReadLine();
+        public int idade = int.Parse(Console.ReadLine());
+
+    }
+}
