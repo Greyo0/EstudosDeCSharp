b943ba96212902953ad6ed7e23c179470b887af2
Arthur Medeiros <arthur.medeiros@hdtec.local>
2026-04-29 15:31:48 -0300

    1149

--- Beecrowd/Iniciante/I1149/Program.cs
+++ Beecrowd/Iniciante/I1149/Program.cs
@@ -6,7 +6,27 @@ public class Program
     public static void Main(string[] args)
     {
 
+        string[] entrada = Console.ReadLine().Split(' ');
 
+        int a = int.Parse(entrada[0]);
+        int n;
+        int sum = 0;
+
+        for (int i = 1; i < entrada.Length(); i++)
+        {
+            if (entrada[i] > 0)
+            {
+              n = int.Parse(entrada[i]);
+              break;  
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += a + i;
+        }
+
+        Console.WriteLine(sum);
 
     }
 
