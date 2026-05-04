e401ce7e0c98552fd7f438f0e5cc2161c57952be
Arthur Medeiros <arthur.medeiros@hdtec.local>
2026-05-04 12:01:44 -0300

    1151

---
+++ Beecrowd/Iniciante/I1151/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        
+        int n = int.Parse(Console.ReadLine());
+        int start = 0;
+        int[] vet = new int[n];
+
+        vet[0] = 0;
+        vet[1] = 1;
+
+        for (int i = 2; i < n; i++)
+        {
+
+            vet[i] = vet[i-1] + vet[i-2]; 
+
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            
+            if (i == n - 1)
+            {
+                Console.WriteLine(vet[i]);
+            } else
+            {
+                Console.Write($"{vet[i]} ");
+            }
+
+        }
+        
+
+    }
+}
