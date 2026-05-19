0094ccca3d7a644655020d1f6268d658a35a9239
Greyo0 <arthurrodriguesgrey@gmail.com>
2026-05-18 22:41:53 -0300

    1156 e 1157

---
+++ Beecrowd/Iniciante/I1156/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization; 
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        int num = 1;
+        double sum = 0;
+
+        for (int i = 1; i <=39; i += 2)
+        {
+            sum += (double)i / num; 
+            num *= 2;
+        }
+
+        Console.WriteLine(sum.ToString("F2"), CultureInfo.InvariantCulture);
+
+    } 
+
+}
