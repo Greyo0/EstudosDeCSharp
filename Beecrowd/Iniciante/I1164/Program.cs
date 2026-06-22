0266b9fef4840236ed8260ad57e21cff87702222
Greyo0 <arthurrodriguesgrey@gmail.com>
2026-06-22 16:59:16 -0300

    I1164

---
+++ Beecrowd/Iniciante/I1164/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program
+{
+    
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int j = 1; j < num; j++)
+            {
+                if (num%j == 0)
+                {
+                    sum += j;
+                }
+            }
+            if (sum == num)
+            {
+                Console.WriteLine($"{num} eh perfeito");
+            } else
+            {
+                Console.WriteLine($"{num} nao eh perfeito");
+            }
+        }
+    }
+
+    
+}
